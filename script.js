5c273d164a6897056b50d4dbaf43e4d84776ae4c
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-02-09 07:51:14 +0100

    todo: cotinue from 3:30

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-const startButtonSelector = ".button__start"
-const stopButtonSelector = ".button__stop"
-const hoursInputSelector = "#hours"
-const minutesInputSelector = "#minutes"
-const secondsInputSelector = "#seconds"
+const startButtonSelector = ".button__start";
+const stopButtonSelector = ".button__stop";
+const hoursInputSelector = "#hours";
+const minutesInputSelector = "#minutes";
+const secondsInputSelector = "#seconds";
 
 
-const startButton = document.querySelector(startButtonSelector)
-const stopButton = document.querySelector(stopButtonSelector)
-const hoursInput = document.querySelector(hoursInputSelector)
-const minutesInput = document.querySelector(minutesInputSelector)
-const secondsInput = document.querySelector(secondsInputSelector)
+const startButton = document.querySelector(startButtonSelector);
+const stopButton = document.querySelector(stopButtonSelector);
+const hoursInput = document.querySelector(hoursInputSelector);
+const minutesInput = document.querySelector(minutesInputSelector);
+const secondsInput = document.querySelector(secondsInputSelector);
+
+let remainingTime;
+let hours;
+let minutes;
+let seconds;
 
 function startTimer(event){
-    event.preventDefault()
+    event.preventDefault();
+
+    hours = parseInt(hoursInput.value);
+    minutes = parseInt(minutesInput.value);
+    seconds = parseInt(secondsInput.value);
+    remainingTime = hours * 3600 + minutes * 60 + seconds;
 }
 
 function updateTimer(){
+    remainingTime = remainingTime - 1
 
 }
 
@@ -24,5 +35,5 @@ function stopTimer(){
 }
 
 
-startButton.addEventListener("click",startTimer)
-stopButton.addEventListener("click",stopTimer)
+startButton.addEventListener("click",startTimer);
+stopButton.addEventListener("click",stopTimer);
