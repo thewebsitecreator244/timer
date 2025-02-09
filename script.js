6d6e59df124a3c2e43c88c7a0c24899d8de98d84
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-02-09 07:20:03 +0100

    feat: add selector

---
+++ script.js
@@ -0,0 +1,5 @@
+const startButtonSelector = ".button__start"
+const stopButtonSelector = ".button__stop"
+const hoursInputSelector = "#hours"
+const minutesInputSelector = "#minutes"
+const secondsInputSelector = "#seconds"
