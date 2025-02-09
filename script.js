f7fad0e7afaf5c7d6ad900cf5b9c2f5a6f7ce4ef
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-02-09 07:26:49 +0100

    feat: found const element

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const stopButtonSelector = ".button__stop"
 const hoursInputSelector = "#hours"
 const minutesInputSelector = "#minutes"
 const secondsInputSelector = "#seconds"
+
+
+const startButton = document.querySelector(startButtonSelector)
+const stopButton = document.querySelector(stopButtonSelector)
+const hoursInput = document.querySelector(hoursInputSelector)
+const minutesInput = document.querySelector(minutesInputSelector)
+const secondsInput = document.querySelector(secondsInputSelector)
+
